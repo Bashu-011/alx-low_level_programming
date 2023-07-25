49cfa27d29fc9013e3f338fea3fb6d32bc01cac2
Bashu-011 <bashu2609@gmail.com>
2023-07-25 12:50:30 -0700

    This is a new task

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,4 +1,4 @@
-OB#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
@@ -12,14 +12,17 @@ int main(void)
 {
 int pass[100];
 int i, sum, n;
-sum = 0;
+
+sum = 0;	
+
 srand(time(NULL));
+
 for (i = 0; i < 100; i++)
 {
 pass[i] = rand() % 78;
 sum += (pass[i] + '0');
 putchar(pass[i] + '0');
-if ((2772 - sum)-'0' < 78)
+if ((2772 - sum) - '0' < 78)
 {
 n = 2772 - sum - '0';
 sum += n;
@@ -27,5 +30,6 @@ putchar(n + '0');
 break;
 }
 }
+
 return (0);
 }
